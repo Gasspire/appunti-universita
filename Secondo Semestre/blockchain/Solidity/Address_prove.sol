ff95e34f90b0d73eef830af0e809d46e98b32343
Gasspire <petroniopetroniodavide@gmail.com>
2026-06-14 11:14:21 +0200

    Auto-sync: 2026-06-14 11:14:21

--- Secondo Semestre/blockchain/Solidity/Address_prove.sol
+++ Secondo Semestre/blockchain/Solidity/Address_prove.sol
@@ -1,3 +1,20 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract AddrContract{
+
+    address Owner =0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    address myAddr = address(this);
+
+    //Funzione che permette  di pagare un contratto
+    receive() external payable{ 
+    }
+
+
+    //Il bilancio viene restituito solo se è l'owner del contratto, altrimenti fatti gli affari tua
+    function getBalance() public view returns(uint){
+        if(Owner == msg.sender) return address(this).balance;
+        return 0;
+    }
+}
---
+++ Secondo Semestre/blockchain/Solidity/Var_Type.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract VarType{
+    uint myData; //Questa è una variabile di STATO, sarà salvata all'interno della memoria del contratto
+
+
+    
+
+
+}
