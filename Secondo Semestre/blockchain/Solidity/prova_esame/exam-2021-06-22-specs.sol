8738e99333aa39c8e1dfe383203d807e3f6dc034
Gasspire <petroniopetroniodavide@gmail.com>
2026-06-17 11:08:20 +0200

    Auto-sync: 2026-06-17 11:08:20

--- Secondo Semestre/blockchain/Solidity/prova_esame/exam-2021-06-22-specs.sol
+++ Secondo Semestre/blockchain/Solidity/prova_esame/exam-2021-06-22-specs.sol
@@ -44,7 +44,12 @@ interface TrustworthyRockPaperScissorsTournamentSpecs {
     function moveScissor() external payable;
     function disputedMatches() external returns (uint8);
 
-
+    struct Pstruct{
+        uint8 id;
+        address payable Player;
+        uint8 num_wins;
+        Move[] mosse;
+    }
     enum Move{Rock, Paper, Scissor}
     enum Player {First, Second}
     event MatchWonBy(Player winner, uint8 numMatch);
@@ -57,15 +62,18 @@ contract TrustworthyRockPaperScissorsTournament is TrustworthyRockPaperScissorsT
     address payable secondPlayer;
     uint8 targetWins;
     uint256 singleMatchFee;
-    struct Pstruct{
-        uint8 id;
-        address payable Player;
-        uint8 num_wins;
-        Move[] mosse;
-    }
     mapping(Player => Pstruct ) giocatori;
     
 
+    modifier OnlyPlayer(){
+        require(msg.sender == firstPlayer || msg.sender == secondPlayer, "Solo i giocatori possono fare delle mosse");
+        _;
+    }
+
+    modifier suffFee(){
+        require(msg.value >= singleMatchFee,"Importo non valido, prova a dare una fee maggiore!");
+        _;
+    }
 
     constructor(address payable _first, address payable _second, uint8 n_target, uint256 fee){
         firstPlayer = payable(_first);
@@ -77,12 +85,12 @@ contract TrustworthyRockPaperScissorsTournament is TrustworthyRockPaperScissorsT
         require(firstPlayer != secondPlayer, "I giocatori devono essere diversi");
         require(targetWins > 0, "Il numero di match da giocare deve essere maggiore di 0");
 
-        Pstruct storage first;
+        Pstruct memory first;
         first.id = 1;
         first.Player = firstPlayer;
         first.num_wins = 0;
 
-        Pstruct storage second;
+        Pstruct memory second;
         second.id = 2;
         second.Player = secondPlayer;
         second.num_wins = 0;
@@ -92,16 +100,50 @@ contract TrustworthyRockPaperScissorsTournament is TrustworthyRockPaperScissorsT
         giocatori[Player.Second] = second;
     }
 
+    function whoPlaying(address sender) internal view returns(Player){
+        if(sender == firstPlayer) return Player.First;
+        else return Player.Second;
+    }
+
+    function checkMatch() internal {
+        //ottengo il numero di match da controllare, si considera sempre l'ultimo dato che tanto si controlla ad ogni mossa
+        uint8 num_match = uint8(giocatori[Player.First].mosse.length < giocatori[Player.Second].mosse.length ? giocatori[Player.First].mosse.length: giocatori[Player.Second].mosse.length) -1;
+        if(num_match <)
+        if((giocatori[Player.First].mosse[num_match] == Move.Paper && giocatori[Player.Second].mosse[num_match] == Move.Rock) ||
+            (giocatori[Player.First].mosse[num_match] == Move.Scissor && giocatori[Player.Second].mosse[num_match] == Move.Paper) || 
+            (giocatori[Player.First].mosse[num_match] == Move.Rock && giocatori[Player.Second].mosse[num_match] == Move.Scissor)){
+                emit MatchWonBy(Player.First, num_match);
+                giocatori[Player.First].num_wins++;
+                if(giocatori[Player.First].num_wins == targetWins) emit TournamentWonBy(Player.First);
+            }
+        else if((giocatori[Player.Second].mosse[num_match] == Move.Paper && giocatori[Player.First].mosse[num_match] == Move.Rock) ||
+            (giocatori[Player.Second].mosse[num_match] == Move.Scissor && giocatori[Player.First].mosse[num_match] == Move.Paper) || 
+            (giocatori[Player.Second].mosse[num_match] == Move.Rock && giocatori[Player.First].mosse[num_match] == Move.Scissor)){
+                emit MatchWonBy(Player.First, num_match);
+                giocatori[Player.First].num_wins++;
+                if(giocatori[Player.First].num_wins == targetWins) emit TournamentWonBy(Player.First);
+            }
+    }
 
 
+    function ret_pFirst()public view returns(address, uint8, uint8){
+        return (giocatori[Player.Second].Player, giocatori[Player.Second].id, giocatori[Player.Second].num_wins); 
+    }
 
-    function moveRock() external payable{}
+    function moveRock() external OnlyPlayer suffFee payable{
+        //individuo chi sta giocando
+        Player currPlayer = whoPlaying(msg.sender);
+        //faccio la mossa
+        giocatori[currPlayer].mosse.push(Move.Rock);
+        //controllo se è stato vinto un match
+
+    }
     function movePaper() external payable{}
     function moveScissor() external payable{}
     function disputedMatches() external returns (uint8){}
 
 
 
-
+    //0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 3, 0
 
 }
