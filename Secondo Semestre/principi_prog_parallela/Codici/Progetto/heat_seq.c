c8a2257666996a98b556aefc3ca4229b548e35eb
Gasspire <petroniopetroniodavide@gmail.com>
2026-06-29 15:27:59 +0200

    Auto-sync: 2026-06-29 15:27:59

---
+++ Secondo Semestre/principi_prog_parallela/Codici/Progetto/heat_seq.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+Fase 1: Creazione della matrice N x N (ho bisogno di un define N così da poter cambiare velocemente in caso) e porla = 0
+Fase 2: Inserimento dei valori a contorno (non so se abbia importanza che li mettiamo solo nelle ultime righe/colonne)
+Fase 3: Calcolo dell'irradiazione da capire come farlo perché non so quante iterazioni (dubito di poter calcolare tutto in una volta)
+        A meno di calcolare diciamo a "rettangolo"
+Fase 4: Calcolo della Norma L2 tra due iterazioni diverse, se questa è inferiore ad una certa soglia FERMA altrimenti riparti da 2.
+*/
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    //Creazione e inizializzazione della matrice
+
+    
+
+    return 0;
+}
